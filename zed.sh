2c917d82891c102d127fdb1439dc17d1565f1b2a
Matteo <mattegiorgi@gmail.com>
2024-07-26 00:31:59 +0200

    rm subl, add zed

---
+++ zed.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# Zed reset with a simple configuration.
+# ---
+# Zed is not included in the Debian repos,
+# install it separately: https://zed.dev/.
+
+
+
+
+### Check & Funcs
+#################
+
+RED='\033[1;36m'
+NC='\033[0m'
+# ---
+if [[ ! -x "$(command -v "zed")" ]]; then
+    printf "\n${RED}%s${NC}"   "═══════ Warning: Zed not found ══════"
+    printf "\n${RED}%s${NC}\n" "Install Zed and run this script again"
+    exit 1
+fi
+# ---
+function error-echo () {
+    printf "${RED}ERROR: %s${NC}\n" "$1" >&2
+    exit 1
+}
+
+
+
+
+### Path & Dependencies
+#######################
+
+SCRIPTPATH="$( cd "$(command dirname "$0")" ; pwd -P )" || exit 1
+command sudo apt-get install -qq -y gnome-keyring bash dash python3 black \
+      golang-go gopls fonts-firacode || error-echo "installing from apt"
+
+
+
+
+### Start
+#########
+
+BASE="${HOME}/.config/zed" && command mkdir -p "${BASE}"
+cat "${SCRIPTPATH}/zed/settings.json" > "${BASE}/settings.json"
+cat "${SCRIPTPATH}/zed/keymap.json" > "${BASE}/keymap.json"
+
+
+
+
+### Finish
+##########
+
+printf "\n${RED}%s${NC}\n" "setup complete"
